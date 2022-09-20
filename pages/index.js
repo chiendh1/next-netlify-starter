3d459121ab04f88b162d512574ebaccd53119334
Chien Do <hongchien192@gmail.com>
2022-09-20 22:23:28 +0700

    change content

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Sự kiện Việt Tiệp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Website đang trong thời gian bảo trì, vui lòng quay lại sau!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Liên hệ: VIET TIEP MEDIA <b>0944880202</b>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
